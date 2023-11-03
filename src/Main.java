7eba0684f8d297f7a3da9ff08779b2701aee4434
Зейнеп <zepaalapaeva@gmail.com>
2023-11-03 14:01:19 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+Triangle triangle = new Triangle(0.5,2,2);
+        System.out.println( " obsh ploshyd : " + triangle.area());
+
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,19 @@
+public class Triangle {
+    double a ;
+    double b ;
+    double c ;
+
+
+
+  public Triangle(double a,int b ,int c){
+     this.a = a;
+     this.b = b;
+     this.c = c;
+
+  }
+  public double area(){
+      return 0.5 * b * c;
+  }}
+
+
+
